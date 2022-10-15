d6b74ead4062d7eefea108e34c6ac4b9980702c8
bernard <bernard.carvalho@ifto.edu.br>
2022-10-14 21:26:32 -0300

    alteração para valor padrão do carrinho

--- pw2/src/main/java/br/edu/ifto/estudante/pw2/Controllers/ConfiguracaoSpringMVC.java
+++ pw2/src/main/java/br/edu/ifto/estudante/pw2/Controllers/ConfiguracaoSpringMVC.java
@@ -1,14 +1,24 @@
 package br.edu.ifto.estudante.pw2.Controllers;
 
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import br.edu.ifto.estudante.pw2.Entities.Venda;
+import br.edu.ifto.estudante.pw2.Repositories.VendaRepository;
+
 @Configuration
 public class ConfiguracaoSpringMVC implements WebMvcConfigurer {
+    
+
     @Override
     public void addViewControllers(ViewControllerRegistry registry){
         registry.addViewController("/").setViewName("index");
-        registry.addViewController("/home").setViewName("index");        
+        registry.addViewController("/home").setViewName("index"); 
     }
 }
